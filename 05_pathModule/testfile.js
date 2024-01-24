bed212515e10911eb58a0f2a32f42a8f441a04fd
Edgar V <edgar.valdezolmos@mx.bosch.com>
2024-01-24 13:52:58 -0600

    test 3

--- 05_pathModule/testfile.js
+++ 05_pathModule/testfile.js
@@ -2,3 +2,6 @@ let text = "Este es un test para probar Git";
 console.log(text);
 text = "Más cambios para seguir probando";
 console.log(text);
+
+text = "Más mas mas y maaaaas";
+console.log(text);
