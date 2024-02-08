2b3fcc225f67c2cb1f64815060ff8fd3d695a3d5
Edgar V <edgar.valdezolmos@mx.bosch.com>
2024-02-08 10:39:33 -0600

    responses

--- 11_HTTP_server/index.js
+++ 11_HTTP_server/index.js
@@ -1,8 +1,22 @@
 const http = require("node:http");
+const fs  = require("node:fs");
 
 const server = http.createServer((req,res) => {
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end("Hello world");
+    //Sending plain text as response
+    //res.writeHead(200, {"Content-Type": "text/plain"});
+    //res.end("Hello world");
+
+    //Sending JSON as response
+    // const superHero = {
+    //     firstName: "Eddie",
+    //     lastName: "Valdi",
+    // };
+    // res.writeHead(200, {"Content-Type": "application/json"});
+    // res.end(JSON.stringify(superHero));
+
+    //Sending HTML as response
+    res.writeHead(200, {"Content-Type": "text/html"});
+    fs.createReadStream(__dirname + "/index.html").pipe(res);
 });
 
 server.listen(3000, () => {
